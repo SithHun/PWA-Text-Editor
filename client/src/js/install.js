a944742e1ec48f3e4582d07e368119da52c8e23c
Seth Sun <sithsun@gmail.com>
2023-07-25 23:33:51 -0400

    JS for install button and eventlister

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,8 +1,14 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
+let deferredPrompt; // "beforeinstallprompt" event
+
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault(); // Prevent the default "Add to Home Screen" prompt
+    deferredPrompt = event; // Save the event for later use
+    butInstall.style.display = 'block'; // Show the install button
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
